7dbfddbf6858f2b99ec58e93b21f480b8c5ba199
Indrė <90854409+IndrePet@users.noreply.github.com>
2022-04-07 00:18:37 +0300

    js documentation

--- index.js
+++ index.js
@@ -8,6 +8,11 @@ const blurSlider = document.querySelector('#blur');
 
 let apiPage = 1;
 
+/**
+ * Imports the image list from API (Lorem picsum) and sends it to `loadThumbnails` function
+ * @param {number} apiPage Number of the page images have to be fetched from
+ * @param {number} limit Number of images that need to be fetched per request `default = 4`
+ */
 const getImageList = (apiPage, limit = 4) => {
   fetch(`https://picsum.photos/v2/list?page=${apiPage}&limit=${limit}`)
     .then((response) => response.json())
@@ -16,7 +21,17 @@ const getImageList = (apiPage, limit = 4) => {
     });
 };
 
+/**
+ * Renders the page sidebar.
+ * - Creates a gallery of images,
+ * - Prevents right-click on them,
+ * - Listens to 'click' events on created images,
+ *    - Calls function `toLarge` to enlarge selected image,
+ *    - Listens to filter input and calls functions to render a enlarged image
+ * @param {object} data parsed JSON object
+ */
 const loadThumbnails = (data) => {
+  console.log(typeof data);
   const imgThumb = document.createElement('img');
   imgThumb.setAttribute('src', data.download_url);
   imgThumb.classList.add('thumbnail');
@@ -41,6 +56,10 @@ const loadThumbnails = (data) => {
   });
 };
 
+/**
+ * A class to create an object meant to render the enlarged image panel and apply selected filters to the image
+ */
+
 class RightSide {
   constructor(data) {
     this.id = data.id;
@@ -50,6 +69,14 @@ class RightSide {
     this.url = `https://picsum.photos/id/${this.id}/${this.width}/${this.height}`;
   }
 
+  /**
+   * Renders the right side panel image and modifies the image source
+   * url according to Lorem Picsum documentation based on the user's
+   * selected filter values
+   *
+   * Prevents image from being saved by disabling right click
+   * @param {number} blurValue A value of range type input
+   */
   toLarge(blurValue) {
     if (greyscale.checked && parseInt(blurValue) === 0) {
       this.url = `https://picsum.photos/id/${this.id}/${this.width}/${this.height}?grayscale`;
@@ -70,6 +97,11 @@ class RightSide {
     imgLarge.addEventListener('contextmenu', (event) => event.preventDefault());
   }
 
+  /**
+   * Resets the right side panel image in order to prepare it for
+   * a new render
+   * @param {number} blurValue Range type input's value passed to the new render `default = 0`
+   */
   newRender(blurValue = 0) {
     containerRight.innerHTML = '';
     this.toLarge(blurValue);
@@ -78,9 +110,11 @@ class RightSide {
 
 getImageList();
 
-/*
-Responsive infinite scroll
-*/
+/**
+ * Event listener to create a responsive infinite scroll.
+ * - Vertical infinite scroll on large screen devices
+ * - Horizontal infinite scroll on small screen devices
+ */
 containerLeft.addEventListener('scroll', () => {
   let imageLimit = 1;
   if (window.innerWidth > 800) {
